893b60ec80b8e5b37fc3d961bb1012dda35943ac
HsOjo <1134031392@qq.com>
2020-05-20 12:13:40 +0800

    Update config example.

--- config.example.py
+++ config.example.py
@@ -1,10 +1,9 @@
-COURSE_CONFIG = {
-    'host': '',
-    'auth': ('', ''),
-    'protocol': 'http',
-}
-
+COURSE_HOST = ''
+COURSE_PROTOCOL = 'http'
 COURSE_SYNC_INTERVAL = 86400
+# Basic Auth
+COURSE_AUTH = None
+# COURSE_AUTH=('<username>', '<password>')
 
 DB_HOST = '127.0.0.1'
 DB_PORT = 3306
@@ -12,6 +11,10 @@ DB_USER = 'root'
 DB_PASSWORD = ''
 DB_DATABASE = ''
 DB_CHARSET = 'utf8mb4'
+
+COURSE_CONFIG = dict(
+    host=COURSE_HOST, auth=COURSE_AUTH, protocol=COURSE_PROTOCOL
+)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://%s:%s@%s:%d/%s?charset=%s' % (
