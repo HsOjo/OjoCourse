24a804716bebefd0a80d1a9cdecd56a99d2847e4
HsOjo <1134031392@qq.com>
2020-05-28 19:45:35 +0800

    Support add admin.

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from flask_migrate import MigrateCommand
 from flask_script import Manager, Shell
 
 from app import app, db
+from app.admin import AdminModel
 from app.course import CourseModel
 from app.user import UserModel, UserInfoModel
 
@@ -12,8 +13,27 @@ manager.add_command('shell', Shell(
         CourseModel=CourseModel,
         UserModel=UserModel,
         UserInfoModel=UserInfoModel,
+        AdminModel=AdminModel,
         db=db,
     )))
+
+
+@manager.command
+def add_admin():
+    username = input('Username:')
+    user = UserModel.query.filter_by(username=username).first()  # type: UserModel
+    if user is not None:
+        admin = AdminModel.query.filter_by(user_id=user.id).first()
+        if admin is None:
+            admin = AdminModel(user_id=user.id)
+            db.session.add(admin)
+            db.session.commit()
+            print('Admin is added.')
+        else:
+            print('Admin is existed.')
+    else:
+        print('User is not exist.')
+
 
 if __name__ == '__main__':
     manager.run()
